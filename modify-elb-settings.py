701b3df41e3466da5a5c1e38d8779887c66f0107
Yennapusa, Ramakanth 000 <Yennapusa.Ramakanth@corp.sysco.com>
2020-02-29 19:40:57 -0600

    add comments

--- modify-elb-settings.py
+++ modify-elb-settings.py
@@ -1,6 +1,9 @@
 import boto3
 
+# get elbv2 client
 client = boto3.client('elbv2')
+
+# modify the attributes of the loab balancer
 response = client.modify_load_balancer_attributes(
     LoadBalancerArn='arn:aws:elasticloadbalancing:us-east-1:117613187320:loadbalancer/app/ot-api-alb/60569c6e14d2e816',
     Attributes=
